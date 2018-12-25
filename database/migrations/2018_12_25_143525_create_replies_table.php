6faf6f214603aab348066cd38c4dff87b55fc188
Kandukuri Charan tej <charantej.kandukurizen3@outlook.com>
2018-12-25 22:54:18 +0530

    migrations, models, controllers. factories, seeders

---
+++ database/migrations/2018_12_25_143525_create_replies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            //LINCKED COLUMNS
+            $table->integer('question_id');
+            $table->integer('user_id');
+            //DELETE ALL THE REPLIES WHEN THE CONNECTED QUESTION IS DELETED
+            $table->foreign('question_id')->reference('id')->on('questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Model\Question::class, 30)->create();
+    }
+}
